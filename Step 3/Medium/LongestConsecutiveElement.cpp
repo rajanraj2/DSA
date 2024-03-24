23465bd8bb1071d31391c6bffd251ce3f1d9243c
BabyMonk <rans2203@gmail.com>
2024-03-24 16:59:05 +0530

    Update step 3 medium problem

--- Step 3/Medium/LongestConsecutiveElement.cpp
+++ Step 3/Medium/LongestConsecutiveElement.cpp
@@ -1,23 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int longestSuccessiveElements(vector<int>&a) {
+//     int n = a.size();
+//     if (n==0) return 0;
+//     sort(a.begin(), a.end());
+//     int lastSmaller = INT_MIN;
+//     int cnt = 0;
+//     int longest = 1;
+//     for (int i=0; i<n; i++){
+//         if (a[i] - 1 == lastSmaller){
+//             cnt += 1;
+//             lastSmaller = a[i];
+//         }
+//         else if (lastSmaller != a[i]){
+//             cnt =  1;
+//             lastSmaller = a[i];
+//         }
+//         longest = max(longest, cnt);
+//     }
+//     return longest;
+// }
+
+// Alternate method 2 : optimal under certain circumstances
 int longestSuccessiveElements(vector<int>&a) {
-    if (a.size()==0) return 0;
-    sort(a.begin(), a.end());
     int n = a.size();
-    int lastSmaller = INT_MIN;
-    int cnt = 0;
+    if (n==0) return 0;
     int longest = 1;
-    for (int i=0; i<n; i++){
-        if (a[i] - 1 == lastSmaller){
-            cnt += 1;
-            lastSmaller = a[i];
+    unordered_set<int> st;
+    for (int i=0; i<n; i++) {
+        st.insert(a[i]);
+    }
+    for (auto it : st) {
+        if (st.find(it - 1) == st.end()) {
+            int cnt = 1;
+            int x = it;
+            while (st.find(x+1) != st.end()) {
+                x ++;
+                cnt ++;
+            }
+            longest = max(longest, cnt);
         }
-        else if (lastSmaller != a[i]){
-            cnt =  1;
-            lastSmaller = a[i];
-        }
-        longest = max(longest, cnt);
     }
     return longest;
 }
