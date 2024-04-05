6b9b821973356aa98b242f6c8ba5ef6b9eb55c3f
Ruggy Mango <rans2203@gmail.com>
2024-04-05 11:49:50 +0530

    Update step 3 hard problems

--- Step 3/Hard/MergeSortedArray.cpp
+++ Step 3/Hard/MergeSortedArray.cpp
@@ -34,29 +34,67 @@ using namespace std;
 
 
 // Alternate method 2 : optimal 1
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int left = m - 1;
+//         int right = 0;
+//         while (left >= 0 && right < n) {
+//             if (nums1[left] > nums2[right]) {
+//                 swap(nums1[left], nums2[right]);
+//                 left--; right++;
+//             }
+//             else {
+//                 break;
+//             }
+//         }
+//         sort(nums1.begin(), nums1.begin() + m);
+//         sort(nums2.begin(), nums2.end());
+//         for (int i = m + n - 1; i >= m; i--) {
+//             nums1[i] = nums2[i - m];    
+//         }
+//     }
+// };
+
+
+// Alternate method 3 : optimal 2
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left = m - 1;
-        int right = 0;
-        while (left >= 0 && right < n) {
-            if (nums1[left] > nums2[right]) {
-                swap(nums1[left], nums2[right]);
-                left--; right++;
-            }
-            else {
-                break;
-            }
+    void swapIfGreater(vector<int>& nums1, int ind1, vector<int>& nums2, int ind2){
+        if (nums1[ind1] > nums2[ind2]) {
+            swap(nums1[ind1], nums2[ind2]);
         }
-        sort(nums1.begin(), nums1.begin() + m);
-        sort(nums2.begin(), nums2.end());
+    }
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = (m + n);
+        int gap = (len / 2) + (len % 2);
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                // arr 1 and arr 2
+                if (left < m && right >= m) {
+                    swapIfGreater(nums1, left, nums2, right - m);
+                }
+                // arr 2 and arr 2
+                else if (left >= m) {
+                    swapIfGreater(nums2, left - m, nums2, right - m);
+                }
+                // arr 1 and arr 1
+                else {
+                    swapIfGreater(nums1, left, nums1, right);
+                }
+                left++; right++;
+            }
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
+        }
         for (int i = m + n - 1; i >= m; i--) {
-            nums1[i] = nums2[i - m];    
+            nums1[i] = nums2[i - m];
         }
     }
 };
-
-// Alternate method 3 : optimal 2
 
 int main(){
     Solution s;
