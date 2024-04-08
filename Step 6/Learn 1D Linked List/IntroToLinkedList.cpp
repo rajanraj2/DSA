abede80a56c4c45147b7d27e706979301f8c4756
Ruggy Mango <rans2203@gmail.com>
2024-04-08 15:37:09 +0530

    Add step 6 Linked List problems

---
+++ Step 6/Learn 1D Linked List/IntroToLinkedList.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node() : data(0), next(nullptr) {}
+    Node(int x) : data(x), next(nullptr) {}
+    Node(int x, Node* next) : data(x), next(next) {}
+ };
+ 
+
+Node* constructLL(vector<int>& arr) {
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    Node* head = constructLL(arr);
+    cout << head->data << endl;
+    return 0;
+}
