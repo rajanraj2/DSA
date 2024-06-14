ac47e5be5ccba6ca59a50beda104c8113c41ea29
Ruggy Mango <rans2203@gmail.com>
2024-06-14 23:47:28 +0530

    Add step 10 medium problems

---
+++ Step 10/Medium/MaxConsecutiveOnesIII.cpp
@@ -0,0 +1,55 @@
+// Link - https://leetcode.com/problems/max-consecutive-ones-iii/
+// Max Consecutive Ones III
+
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0, right = 0;
+        int zero_count = 0;
+        int result = 0;
+
+        while (right < nums.size()) {
+            if (nums[right] == 0) zero_count++;
+
+            while (zero_count > k) {
+                if (nums[left] == 0) {
+                    zero_count--;
+                }
+                left++;
+            }
+
+            result = max(result, right - left + 1);
+            right++;
+        }
+        return result;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int longestOnes(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int count0 = 0, length = 0, result = 0;
+//         int index0;
+//         for (int i  = 0; i < n; i++) {
+//             int start = i;
+//             if ((nums[i] == 0) && (count0 < k)) {
+//                 if (count0 == 1) start = i;
+//                 count0++;
+//                 length++;
+//             }
+//             else if (nums[i] == 1) {
+//                 length++;
+//             }
+//             else {
+//                 if (count0 > 0) i = start;
+//                 length = 0;
+//                 count0 = 0;
+//             }
+//             result = max(result, length);
+//         }
+//         return result;
+//     }
+// };
