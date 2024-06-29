2fb1ba3747769e08ac8871a4f6e671ec59aab3b4
Ruggy Mango <rans2203@gmail.com>
2024-06-29 14:14:46 +0530

    Add recursion problems from TUF Yt till L7

---
+++ TUF_Recursion/Print_all_subsequences.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i, vector<int> &temp, int n, vector<int> arr) {
+    if (i >= n) {
+        for (int j = 0; j < temp.size(); j++) {
+            cout << temp[j] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    temp.push_back(arr[i]);
+    f(i + 1, temp, n, arr); // take
+    temp.pop_back();
+    f(i + 1, temp, n, arr); // not take
+}
+
+int main() {
+    vector<int> arr = {3, 1, 2};
+    vector<int> temp;
+    f(0, temp, arr.size(), arr);
+    return 0;
+}
