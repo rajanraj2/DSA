975af600fc5195d4aef328f9c0269f352215a95a
Ruggy Mango <rans2203@gmail.com>
2024-09-16 20:26:34 +0530

    Add step 13 lec 3 problems

---
+++ Step 13/Lec_03/02_LCA_BT.cpp
@@ -0,0 +1,26 @@
+// Link - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// Lowest Common Ancestor of a Binary Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == nullptr) return right;
+        else if (right == nullptr) return left;
+        else return root;
+    }
+};
