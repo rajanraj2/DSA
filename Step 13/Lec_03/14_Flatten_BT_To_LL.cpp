e03c46a733566915b9e363e08c3c6058c2c8915e
Ruggy Mango <rans2203@gmail.com>
2024-09-17 21:34:39 +0530

    Add step 13 lec 2 problems

---
+++ Step 13/Lec_03/14_Flatten_BT_To_LL.cpp
@@ -0,0 +1,35 @@
+// Link - https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+// Flatten Binary Tree to Linked List
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        TreeNode* prev = nullptr;
+
+        while (curr) {
+            if (curr->left) {
+                prev = curr->left;
+                while (prev->right) {
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = nullptr;
+            }
+            curr = curr->right;
+        }
+    }
+};
