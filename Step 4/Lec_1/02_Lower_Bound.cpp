5a201894c56fbad0ae9905083725525b52c9f780
rajanraj2 <rajanraj253545@gmail.com>
2024-11-24 16:53:01 +0530

    Add step 4 lec 1 problems

--- Step 4/Lec_1/02_Lower_Bound.cpp
+++ Step 4/Lec_1/02_Lower_Bound.cpp
@@ -2,23 +2,40 @@
 // Floor in a Sorted Array
 
 
+// class Solution {
+//   public:
+
+//     int findFloor(vector<int>& arr, int k) {
+
+//         // Your code here
+//         int lb = lower_bound(arr.begin(), arr.end(), k) - arr.begin();
+//         if (lb == 0 && arr[lb] > k) return -1;
+//         if (lb == arr.size() || arr[lb] > k) lb--;
+//         return lb;
+//         int n = arr.size(), result = -1;
+//         int left = 0, right = n - 1;
+        
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             if (arr[mid] <= k) {
+//                 result = mid;
+//                 left = mid + 1;
+//             }
+//             else right = mid - 1;
+//         }
+//         return result;
+//     }
+// };
+
+
+// STL method 
+
 class Solution {
   public:
-
     int findFloor(vector<int>& arr, int k) {
-
-        // Your code here
-        int n = arr.size(), result = -1;
-        int left = 0, right = n - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] <= k) {
-                result = mid;
-                left = mid + 1;
-            }
-            else right = mid - 1;
-        }
-        return result;
+        int lb = lower_bound(arr.begin(), arr.end(), k) - arr.begin();
+        if (lb == 0 && arr[lb] > k) return -1;
+        if (lb == arr.size() || arr[lb] > k) lb--;
+        return lb;
     }
 };
