b31a07e45a3fc7bf8cfeac34143edb9ea1773055
rajanraj2 <rajanraj253545@gmail.com>
2024-11-24 16:43:47 +0530

    Add step 4 lec 1 problems

---
+++ Step 4/Lec_1/02_Lower_Bound.cpp
@@ -0,0 +1,24 @@
+// https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%253BbatchId=154
+// Floor in a Sorted Array
+
+
+class Solution {
+  public:
+
+    int findFloor(vector<int>& arr, int k) {
+
+        // Your code here
+        int n = arr.size(), result = -1;
+        int left = 0, right = n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= k) {
+                result = mid;
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+        return result;
+    }
+};
