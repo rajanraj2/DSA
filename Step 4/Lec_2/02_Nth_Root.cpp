1e23b0f99137d7e5f98bc6603389b084f810d701
rajanraj2 <rajanraj253545@gmail.com>
2024-11-28 11:53:13 +0530

    Add step 4 lec 2 problems

---
+++ Step 4/Lec_2/02_Nth_Root.cpp
@@ -0,0 +1,43 @@
+// https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1
+// Find Nth root of M
+
+
+class Solution {
+  private:
+    // TC : O(n)
+    int helper(int mid, int n) {
+        int val = 1;
+        for (int i = 0; i < n; i++) val *= mid;
+        return val;
+    }
+    
+    // TC : O(log(n))
+    int power(int mid, int n) {
+        long long ans = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans = ans * mid;
+                n = n - 1;
+            }
+            else {
+                mid = mid * mid;
+                n = n / 2;
+            }
+        }
+        return ans;
+    }
+    
+  public:
+    int nthRoot(int n, int m) {
+        // Code here.
+        int left = 1, right = m;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int val = power(mid, n);
+            if (val == m) return mid;
+            else if (val < m) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+};
