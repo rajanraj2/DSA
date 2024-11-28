74aced0e6de5d4be674ad214558fd5edca15db72
rajanraj2 <rajanraj253545@gmail.com>
2024-11-28 11:59:01 +0530

    Add step 4 lec 2 problems

--- Step 4/Lec_2/02_Nth_Root.cpp
+++ Step 4/Lec_2/02_Nth_Root.cpp
@@ -5,35 +5,37 @@
 class Solution {
   private:
     // TC : O(n)
-    int helper(int mid, int n) {
+    int helper(int mid, int n, int m) {
         int val = 1;
-        for (int i = 0; i < n; i++) val *= mid;
+        for (int i = 0; i < n; i++) {
+            val *= mid;
+            if (val > m) return val; // solves multiplication overflow issue
+        }
         return val;
     }
     
     // TC : O(log(n))
-    int power(int mid, int n) {
-        long long ans = 1;
-        while (n > 0) {
-            if (n % 2 == 1) {
-                ans = ans * mid;
-                n = n - 1;
-            }
-            else {
-                mid = mid * mid;
-                n = n / 2;
-            }
-        }
-        return ans;
-    }
+    // int power(int mid, int n) {
+    //     long long ans = 1;
+    //     while (n > 0) {
+    //         if (n % 2 == 1) {
+    //             ans = ans * mid;
+    //             n = n - 1;
+    //         }
+    //         else {
+    //             mid = mid * mid;
+    //             n = n / 2;
+    //         }
+    //     }
+    //     return ans;
+    // }
     
   public:
     int nthRoot(int n, int m) {
-        // Code here.
         int left = 1, right = m;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            int val = power(mid, n);
+            int val = helper(mid, n, m);
             if (val == m) return mid;
             else if (val < m) left = mid + 1;
             else right = mid - 1;
